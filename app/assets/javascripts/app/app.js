fe13089af3b2e94b3627fd67dc30102b19ffcc6d
Smulligan85 <sean.mulligan85@gmail.com>
2016-09-18 18:32:18 -0700

    Add beginning templates and structure

--- app/assets/javascripts/app/app.js
+++ app/assets/javascripts/app/app.js
@@ -1,2 +1,21 @@
 angular
-  .module('app', ['ui.router', 'ngResource', 'templates']);
+  .module('app', ['ui.router', 'ngResource', 'templates'])
+  .config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'home.html',
+        // controller: 'HomeController as home'
+      })
+      .state ('home.flights', {
+        url: 'flights',
+        templateUrl: 'home/flights.html',
+        // controller: 'FlightsController as flights'
+      })
+      .state('home.notes', {
+        url:'notes',
+        templateUrl: 'home/notes.html',
+        // controller: 'NotesController as notes'
+      });
+      $urlRouterProvider.otherwise('/');
+  });
